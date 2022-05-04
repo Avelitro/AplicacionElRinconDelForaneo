dbfb1fd0e791c251e053d9fd042bc4bc6d614f84
avelitro <joseabelmartinezleyva@gmail.com>
2022-05-03 23:04:09 -0500

    Final

--- proyectoElRinconDelForaneo/src/clases/DatabaseConnection.java
+++ proyectoElRinconDelForaneo/src/clases/DatabaseConnection.java
@@ -31,7 +31,7 @@ public class DatabaseConnection {
     public boolean Conectar() {
         try {
             //Class.forName("com.mysql.jdbc.Driver").newInstance();
-            con = DriverManager.getConnection("jdbc:mysql://localhost/elrincon_delforaneo", "root", "");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/elrincon_delforaneo","root", "");
             return con != null;
         } catch (Exception e) {
             return false;
