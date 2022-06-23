beb0dcdc18dfbb01c132decc8115dc6ec8e861b4
avelitro <joseabelmartinezleyva@gmail.com>
2022-06-23 16:29:46 -0500

    Cambios finales

--- proyectoElRinconDelForaneo/src/entity/usuario.java
+++ proyectoElRinconDelForaneo/src/entity/usuario.java
@@ -15,81 +15,147 @@ import org.apache.commons.codec.binary.Base64;
  */
 
 /**
- *
+ * 
+ * Clase Usuario
+ * 
+ * Contiene informacion de cada usuario
  * @author rheve
  */
 public class usuario {
+    //Atributos 
+    /**
+     * Identificador del usuario
+     */
     private int idUsuario;
+    /**
+     * Nombres del usuario
+     */
     private String nombres;
+    /**
+     * Apellidos del usuario
+     */
     private String apellidoPaterno;
     private String apellidoMaterno;
+    /**
+     * Correo del usuario
+     */
     private String correo;
+    /**
+     * Contraseña del usuario
+     */
     private String contraseña;
+    /**
+     * Tipo de usuario
+     */
     private String tipoUsuario;
-    
+    /**
+     * variable para encriptar contraseña
+     */
     String secretKey = "contrasena";
+    /**
+     * Constructor por defecto
+     */
     public usuario() {
     }
-    
-    
+    //Funciones publicos
+    /**
+     * 
+     * Funcion que retorna el Identificador del usuario
+     * @return
+     * 
+     */
     public int getIdUsuario() {
         return idUsuario;
     }
-
+    /**
+     * Funcion que agrega el Identificador del usuario
+     * @param idUsuario 
+     */
     public void setIdUsuario(int idUsuario) {
         this.idUsuario = idUsuario;
     }
-
+    /**
+     * Funcion que retorna los nombres del usuario
+     * @return 
+     */
     public String getNombres() {
         return nombres;
     }
-
+    /**
+     * Funcion que agrega los nombres del usuario
+     * @param nombres 
+     */
     public void setNombres(String nombres) {
         this.nombres = nombres;
     }
-
+    /**
+     * Funcion que retorna los apellidos del usuario
+     * @return 
+     */
     public String getApellidoPaterno() {
         return apellidoPaterno;
     }
-
-
-    public void setApellidoPaterno(String apellidoPaterno) {
-        this.apellidoPaterno = apellidoPaterno;
-    }
-    
     public String getApellidoMaterno() {
         return apellidoMaterno;
     }
-
-
+    /**
+     * Funcion que agrega los apellidos al usuario
+     * @param apellidoPaterno 
+     * @param apellidoMaterno
+     */
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
     public void setApellidoMaterno(String apellidoMaterno) {
         this.apellidoMaterno = apellidoMaterno;
     }
-
+    /**
+     * Funcion que retorna el correo del usuario
+     * @return 
+     */
     public String getCorreo() {
         return correo;
     }
-
+    /**
+     * Funcion que agrega el correo al usuario
+     * @param correo 
+     */
     public void setCorreo(String correo) {
         this.correo = correo;
     }
-
+    /**
+     * Funcion que retorna la contraseña del usuario
+     * @return 
+     */
     public String getContrasena() {
         return contraseña;
     }
-
+    /**
+     * Funcion que agrega la contraseña al usuario
+     * @param contrasena 
+     */
     public void setContrasena(String contrasena) {
         this.contraseña = contrasena;
     }
-
+    /**
+     * Funcion que retorna el tipo de usuario
+     * @return 
+     */
     public String getTipoUsuario() {
         return tipoUsuario;
     }
-
+    /**
+     * Funcion que agrega el tipo de usuario al usuario
+     * @param tipoUsuario 
+     */
     public void setTipoUsuario(String tipoUsuario) {
         this.tipoUsuario = tipoUsuario;
     }
-    
+    /**
+     * Funcion que encripta la contraseña
+     * @param cadena
+     * @return 
+     */
     public String encriptar(String cadena) {
         String encriptacion = "";
         try {
@@ -108,7 +174,11 @@ public class usuario {
         }
         return encriptacion;
     }
-
+    /**
+     * Funcion que desencripta
+     * @param cadenaEncriptada
+     * @return 
+     */
     public String desencriptar(String cadenaEncriptada) {
         String desencriptacion = "";
         try {
